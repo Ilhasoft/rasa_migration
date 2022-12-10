1d36e81d26ebee9ee6209f4e20d5ea5b71a6fbed
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-10 19:53:45 +0000

    Adding tarfile member sanitization to extractall()

--- test_train.py
+++ test_train.py
@@ -49,7 +49,26 @@ class Migration:
         tar_file.close()
 
         with tarfile.open(tar_file.name, "r:gz") as tar:
-            tar.extractall(model_directory)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, model_directory)
 
         files_directory = [
             f for f in os.listdir(model_directory) if re.search(r'^(metadata|training_data)+\.[json]{4}$', f)
